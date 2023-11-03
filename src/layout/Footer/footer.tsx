0cb07278844b07bbe210a1b5aaa54141c5cc4b62
VormaniJr <nikita_karev@inbox.ru>
2023-11-03 23:57:32 +0300

    Test

--- src/layout/Footer/footer.tsx
+++ src/layout/Footer/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={commonStyles.container}>
-        <h2>Footer site</h2>
+        <h2>Footer</h2>
       </div>
     </footer>
   );
