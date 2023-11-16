ed30a4fad45379817d5fd58115898ad2f9b0bef7
Sh1zome <shizomech@gmail.com>
2023-11-17 04:04:53 +0500

    change names of pages in modal menu

--- src/layout/Header/header.tsx
+++ src/layout/Header/header.tsx
@@ -42,62 +42,62 @@ function Header() {
       >
         <Box className={styles.modal}>
           <Typography variant="h6" component="h2">
-            Menu
+            Каталог страниц
           </Typography>
           <Stack>
             <NavLink to={ROUTES.HOME} onClick={CloseMenu}>
-              Home
+              Главная
             </NavLink>
             <NavLink to={ROUTES.INFO} onClick={CloseMenu}>
-              About
+              Информация
             </NavLink>
             <NavLink to={ROUTES.NEWS} onClick={CloseMenu}>
-              News
+              Новости
             </NavLink>
             <NavLink to={ROUTES.OFFERS} onClick={CloseMenu}>
-              Offers
+              Акции
             </NavLink>
             <NavLink to={ROUTES.SERVICE_CENTER} onClick={CloseMenu}>
-              Service Center
+              Услуги СЦ
             </NavLink>
             <NavLink to={ROUTES.IT_SERVICE} onClick={CloseMenu}>
-              IT Service
+              IT услуги
             </NavLink>
             <NavLink to={ROUTES.SKUD} onClick={CloseMenu}>
-              SKUD
+              Видеонаблюдение/Умный дом/Системы оповещения
             </NavLink>
             <NavLink to={ROUTES.PRINT_3D} onClick={CloseMenu}>
-              Print 3D
+              3D печать
             </NavLink>
             <NavLink to={ROUTES.OWN_PRODUCTS} onClick={CloseMenu}>
-              Own Products
+              Собственные товары
             </NavLink>
             <NavLink to={ROUTES.PRICE_LIST} onClick={CloseMenu}>
-              Price List
+              Прайс-лист
             </NavLink>
             <NavLink to={ROUTES.DELIVERY} onClick={CloseMenu}>
-              Delivery
+              Доставка (Логистика)
             </NavLink>
             <NavLink to={ROUTES.GUARANTEE} onClick={CloseMenu}>
-              Guarantee
+              Гарантии
             </NavLink>
             <NavLink to={ROUTES.GOV_PURCHASES} onClick={CloseMenu}>
-              Gov Purchases
+              Гос. закупки
             </NavLink>
             <NavLink to={ROUTES.FZ_44_223} onClick={CloseMenu}>
-              44 and 223 FZ
+              44 и 223 ФЗ
             </NavLink>
             <NavLink to={ROUTES.DISPOSAL} onClick={CloseMenu}>
-              Disposal
+              Утилизация
             </NavLink>
             <NavLink to={ROUTES.USEFUL_TIPS} onClick={CloseMenu}>
-              Useful Tips
+              Полезные советы
             </NavLink>
             <NavLink to={ROUTES.PARTNERSHIP} onClick={CloseMenu}>
-              Partnership
+              Партнёрство
             </NavLink>
             <NavLink to={ROUTES.CERTIFICATES} onClick={CloseMenu}>
-              Certificates
+              Сертификаты
             </NavLink>
           </Stack>
         </Box>
